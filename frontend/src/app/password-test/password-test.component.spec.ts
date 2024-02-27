f4ead79d821c90837bddf935cbfcd905093e044f
Weine Oliveira <weineolliver@gmail.com>
2024-02-27 15:59:36 -0300

    feat(test): adding tests

--- frontend/src/app/password-test/password-test.component.spec.ts
+++ frontend/src/app/password-test/password-test.component.spec.ts
@@ -1,17 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { PasswordTestComponent } from './password-test.component';
+import { PasswordService } from '../services/password.service';
+import { CommonModule } from '@angular/common';
 
 describe('PasswordTestComponent', () => {
   let component: PasswordTestComponent;
   let fixture: ComponentFixture<PasswordTestComponent>;
+  let passwordServiceSpy: jasmine.SpyObj<PasswordService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PasswordTestComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(waitForAsync(() => {
+    passwordServiceSpy = jasmine.createSpyObj('PasswordService', [
+      'validatePassword',
+    ]);
+
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, CommonModule], // Import the necessary modules here
+      providers: [{ provide: PasswordService, useValue: passwordServiceSpy }],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(PasswordTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +29,46 @@ describe('PasswordTestComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call checkPassword on form submission', () => {
+    spyOn(component, 'checkPassword');
+    component.submitForm();
+    expect(component.checkPassword).toHaveBeenCalled();
+  });
+
+  it('should set passwordIsValid to true if password is valid', () => {
+    const password = 'valid_password';
+    const mockResponse = { isValid: true };
+    passwordServiceSpy.validatePassword.and.returnValue(of(mockResponse));
+
+    component.checkPassword(password);
+
+    expect(component.passwordIsValid).toBe(true);
+    expect(component.passForm.get('password')?.hasError('apiError')).toBe(
+      false
+    );
+  });
+
+  it('should set passwordIsValid to false and set apiError if password is invalid', () => {
+    const password = 'invalid_password';
+    const mockResponse = { isValid: false };
+    passwordServiceSpy.validatePassword.and.returnValue(of(mockResponse));
+
+    component.checkPassword(password);
+
+    expect(component.passwordIsValid).toBe(false);
+    expect(component.passForm.get('password')?.hasError('apiError')).toBe(true);
+  });
+
+  it('should set passwordIsValid to false and set apiError on API error', () => {
+    const password = 'some_password';
+    passwordServiceSpy.validatePassword.and.returnValue(of({ isValid: false }));
+    spyOn(console, 'error');
+
+    component.checkPassword(password);
+
+    expect(component.passwordIsValid).toBe(false);
+    expect(component.passForm.get('password')?.hasError('apiError')).toBe(true);
   });
 });
--- frontend/src/app/services/password.service.spec.ts
+++ frontend/src/app/services/password.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { PasswordService } from './password.service';
 
 describe('PasswordService', () => {
   let service: PasswordService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PasswordService],
+    });
     service = TestBed.inject(PasswordService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should validate password', () => {
+    const mockResponse = { isValid: true };
+    const password = 'your_password';
+
+    service.validatePassword(password).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:3000/password-validator'
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({ password });
+
+    req.flush(mockResponse);
   });
 });
