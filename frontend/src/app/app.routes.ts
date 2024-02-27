01c9bd6f6b2e4be99d744d556e9df3eee4f78928
Weine Oliveira <weineolliver@gmail.com>
2024-02-27 10:38:11 -0300

    feat(routes): creating routes for the app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PasswordTestComponent } from './password-test/password-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PasswordTestComponent },
+  { path: '**', redirectTo: '' },
+];
